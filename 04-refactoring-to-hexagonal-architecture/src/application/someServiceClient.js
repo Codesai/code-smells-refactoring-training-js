40d01332bb3478c579cad067cb950486f9d0b087
freyes <fran@codesai.com>
2023-11-20 16:43:16 +0000

    fix: someServiceClient was using BirthdayService without injecting dependency

--- 04-refactoring-to-hexagonal-architecture/src/application/someServiceClient.js
+++ 04-refactoring-to-hexagonal-architecture/src/application/someServiceClient.js
@@ -1,12 +1,14 @@
 import {OurDate} from "../core/ourDate";
 import {BirthdayService} from "./birthdayService";
+import {FileEmployeesRepository} from "../infrastructure/repositories/fileEmployeesRepository";
 
 export class SomeServiceClient {
 
     runService() {
-        const service = new BirthdayService();
+        const filename = "employee_data.txt";
+        const service = new BirthdayService(new FileEmployeesRepository(filename));
         try {
-            service.sendGreetings("employee_data.txt",
+            service.sendGreetings(filename,
                 new OurDate("2008/10/08"), "localhost", 25);
         } catch (e) {
             console.log(e);
